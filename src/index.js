ef05b8806d6bb57ce99f1f1a58cf807a6cbf0fa4
Orlando <drapezoid@gmail.com>
2023-01-06 01:49:20 +0300

    fixes unnesesary zeros and bugs, correct implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
     let sum = "";
     let stringNumber = String(number);
-    let lastDigitOfNumber = +stringNumber.length - 1;
-    let secondDigitOfNumber = +stringNumber.length - 2;
-    let hundredS = +stringNumber.length - 3;
+    let lastDigitOfNumber = +stringNumber[+stringNumber.length - 1];
+    let secondDigitOfNumber = +stringNumber[+stringNumber.length - 2];
+    let hundredS = +stringNumber[+stringNumber.length - 3];
     let simpleDigits = ['zero','one','two','three','four','five','six','seven','eight','nine'];
     let teenS = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-    let decimalS = ['twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety'];
+    let decimalS = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
     function simpleDigitsFunction(lastDigitOfNumber){
         switch (lastDigitOfNumber) {
         case 0:
@@ -30,9 +30,9 @@ module.exports = function toReadable (number) {
         case 9:
             return simpleDigits[9];
         }
-    }
+    };
     function twoDigitsInNumber(secondDigitOfNumber){
-        if(secondDigitOfNumber != 1){
+        if(secondDigitOfNumber > 1){
             switch (secondDigitOfNumber) {
             case 2:
                 return decimalS[0];
@@ -54,67 +54,73 @@ module.exports = function toReadable (number) {
             else if (secondDigitOfNumber == 1){
                 switch (lastDigitOfNumber) {
                     case 0:
-                    return teenS[0];
+                        return teenS[0];
                     case 1:
-                    return teenS[1];
+                        return teenS[1];
                     case 2:
-                    return teenS[2];
+                        return teenS[2];
                     case 3:
-                    return teenS[3];
+                        return teenS[3];
                     case 4:
-                    return teenS[4];
+                        return teenS[4];
                     case 5:
-                    return teenS[5];
+                        return teenS[5];
                     case 6:
-                    return teenS[6];
+                        return teenS[6];
                     case 7:
-                    return teenS[7];
+                        return teenS[7];
                     case 8:
-                    return teenS[8];
+                        return teenS[8];
                     case 9:
-                    return teenS[9];
+                        return teenS[9];
                 }
             }
-    }
+            else if (secondDigitOfNumber == 0){
+                return '';
+            }
+    };
     function threeDigitsInNumber(hundredS){
         switch (hundredS){
         case 1:
-            return simpleDigits[1] + 'hundred';
+            return (simpleDigits[1] + ' hundred');
         case 2:
-            return simpleDigits[2] + 'hundred';
+            return (simpleDigits[2] + ' hundred');
         case 3:
-            return simpleDigits[3] + 'hundred';
+            return (simpleDigits[3] + ' hundred');
         case 4:
-            return simpleDigits[4] + 'hundred';
+            return (simpleDigits[4] + ' hundred');
         case 5:
-            return simpleDigits[5] + 'hundred';
+            return (simpleDigits[5] + ' hundred');
         case 6:
-            return simpleDigits[6] + 'hundred';
+            return (simpleDigits[6] + ' hundred');
         case 7:
-            return simpleDigits[7] + 'hundred';
+            return (simpleDigits[7] + ' hundred');
         case 8: 
-            return simpleDigits[8] + 'hundred';
+            return (simpleDigits[8] + ' hundred');
         case 9:
-            return simpleDigits[9] + 'hundred';
+            return (simpleDigits[9] + ' hundred');
         }
-    }
+    };
     if (stringNumber.length <= 0 || stringNumber.length > 3){return 'error'}
     else if (stringNumber.length == 1){
-            sum = simpleDigitsFunction;
+            sum = simpleDigitsFunction(lastDigitOfNumber);
     }
     else if (stringNumber.length == 2){
-            if (secondDigitOfNumber == 1){sum = twoDigitsInNumber;}
+            if (secondDigitOfNumber == 1){sum = twoDigitsInNumber(secondDigitOfNumber);}
             else if (secondDigitOfNumber > 1){
-                if (lastDigitOfNumber == 0){sum = twoDigitsInNumber}
-            else {sum = twoDigitsInNumber + simpleDigitsFunction;}
+                if (lastDigitOfNumber == 0){sum = twoDigitsInNumber(secondDigitOfNumber);}
+            else {sum = twoDigitsInNumber(secondDigitOfNumber) + ' ' + simpleDigitsFunction(lastDigitOfNumber);}
             }
     }
     else if (stringNumber.length == 3){
-            if (secondDigitOfNumber == 1){sum = threeDigitsInNumber + twoDigitsInNumber;}
+            if (secondDigitOfNumber == 1){sum = threeDigitsInNumber(hundredS) + ' ' + twoDigitsInNumber(secondDigitOfNumber);}
+            else if (lastDigitOfNumber == 0 && secondDigitOfNumber == 0){sum = threeDigitsInNumber(hundredS)}
+            else if (secondDigitOfNumber == 0){sum = threeDigitsInNumber(hundredS) + ' ' + simpleDigitsFunction(lastDigitOfNumber);}
             else if (secondDigitOfNumber > 1){
-                if (lastDigitOfNumber == 0){sum = threeDigitsInNumber + twoDigitsInNumber}
-            else {sum = threeDigitsInNumber + twoDigitsInNumber + simpleDigitsFunction;}
+            if (lastDigitOfNumber == 0 && secondDigitOfNumber == 0){sum = threeDigitsInNumber(hundredS)}
+            else if (lastDigitOfNumber == 0){sum = threeDigitsInNumber(hundredS) + ' ' + twoDigitsInNumber(secondDigitOfNumber);}
+            else {sum = threeDigitsInNumber(hundredS) + ' ' + twoDigitsInNumber(secondDigitOfNumber) + ' ' + simpleDigitsFunction(lastDigitOfNumber);}
             }
         }
-return sum;
-}
+    return sum;
+};
